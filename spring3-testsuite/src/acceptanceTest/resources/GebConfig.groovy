69715eccb6241c5b0b91332ae7ced8476aa78774
Adrian Kelly <adrianbkelly@gmail.com>
2014-05-09 09:02:29 +1000

    Needs the gradle build task to include the swagger-ui static files.

--- spring3-testsuite/src/acceptanceTest/resources/GebConfig.groovy
+++ spring3-testsuite/src/acceptanceTest/resources/GebConfig.groovy
@@ -4,13 +4,15 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import static org.openqa.selenium.remote.DesiredCapabilities.chrome
 
 waiting {
-  timeout = 10
+
+  //Goggle fonts in swagger ui :(
+  timeout = 12
 }
 baseUrl = "http://localhost:9080/spring3-testsuite/"
 
 driver = {
   def ffDriver = new FirefoxDriver()
-  ffDriver.manage().window().maximize()//.setSize(new Dimension(1280, 1024))
+  ffDriver.manage().window().maximize()
   ffDriver
 }
 
@@ -18,7 +20,7 @@ environments {
   chrome {
     driver = {
       def chromeDriver = new ChromeDriver()
-      chromeDriver.manage().window().maximize()//.setSize(new Dimension(1280, 1024))
+      chromeDriver.manage().window().maximize()
       chromeDriver
     }
   }
@@ -26,7 +28,7 @@ environments {
   firefox {
     driver = {
       def ffDriver = new FirefoxDriver()
-      ffDriver.manage().window().maximize()//.setSize(new Dimension(1280, 1024))
+      ffDriver.manage().window().maximize()
       ffDriver
     }
   }
